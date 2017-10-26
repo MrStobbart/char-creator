5c5d761e6d0b0ac7bb61a51b5e4e3a9cc6d8cb08
Marek Meyer <marek_meyer@outlook.com>
2017-10-26 11:57:57 +0100

    added server.js & first version of char json

---
+++ client/components/charPageNav/charPageNav.js
@@ -0,0 +1,20 @@
+function charPageNav(props) {
+  return (
+    <div className="xlarge-20 large-20 medium-30 hide-small hide-tiny">
+      <nav className="ink-navigation ink-sticky">
+        <ul className="menu vertical grey">
+          <li><a href="#base-data" className="ink-smooth-scroll">Allgemein</a></li>
+          <li><a href="#attributes" className="ink-smooth-scroll">Attribute</a></li>
+          <li><a href="#">Grundwerte</a></li>
+          <li><a href="#">Vorteile/Nachteile</a></li>
+          <li><a href="#">Sonderfertigkeiten</a></li>
+          <li><a href="#">Kampftechniken</a></li>
+          <li><a href="#">Fertigkeiten</a></li>
+          <li><a href="#">Waffen & Rüstungen</a></li>
+          <li><a href="#">Ausrüstung</a></li>
+          <li><a href="#">Zauber/Liturgien</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
