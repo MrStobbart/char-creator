e1fd14fd2049f822bbe7e886c8ddfbf44fff0bf2
Marek Meyer <marek_meyer@outlook.com>
2017-10-26 12:50:28 +0100

    added production file serve with express

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mundanChar = require('./data/theDarkEye/mundanChar');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/')
+if (process.env.NODE_ENV === 'production') {
+  console.log('Serve the production build');
+  app.use(express.static('build'));
+}
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.route('/api/thedarkeye/mundanchar')
+  .get((req, res) => {
+    res.json(mundanChar);
+  });
+
+
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
 })
