7e6d55aec4846bcc4e03fd975c6a8dac5057cb26
Marek Meyer <marek_meyer@outlook.com>
2017-10-31 13:14:56 +0000

    Changed style of fields

--- src/components/CharPageContent/Field.jsx
+++ src/components/CharPageContent/Field.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './Field.css';
 
 export function Field(props) {
   return (
     <div className="column-group quarter-gutters">
       <label className="all-40 align-left" htmlFor="name">{props.children}</label>
       <div className="control all-60">
-        <input type="text" name="name" value="" />
+        <input className={props.type} type="text" name="name" />
       </div>
     </div>
   )
+}
+
+Field.propTypes = {
+  type: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired
 }
