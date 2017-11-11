2f7baefd09fca75ed5736f1a46b05f686c7767a1
Marek Meyer <marek_meyer@outlook.com>
2017-11-11 12:44:49 +0100

    fixed ink grid

--- src/CharPage/components/Navigation/index.js
+++ src/CharPage/components/Navigation/index.js
@@ -10,7 +10,7 @@ export function Navigation(props) {
   }
   
   return (
-    <div className="xlarge-20 large-20 medium-30 hide-small hide-tiny">
+    <div className="xlarge-15 large-20 medium-25 hide-small hide-tiny">
       <nav className="ink-navigation ink-sticky">
         <ul className="menu vertical grey">
           {fieldsets}
