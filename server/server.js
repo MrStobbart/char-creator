11c7cd8cae6350b4406ee6d1c08a14860f921900
Marek Meyer <marek_meyer@outlook.com>
2017-11-19 21:26:06 +0000

    Fixed database in mongo connection string

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const port = 8080;
 
 
 // Middleware: Make mongoDb db object available in req.
-app.use(dbConnectionMiddleware('mongodb://127.0.0.1:27017'));
+app.use(dbConnectionMiddleware('mongodb://127.0.0.1:27017/charCreator'));
 
 // Middleware: Parse request body to json.
 app.use(bodyParser.json());
