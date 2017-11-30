f42a04129418697e9aa1c25527b0160428eabeef
Marek Meyer <marek_meyer@outlook.com>
2017-11-30 18:53:31 +0000

    Fixed build script and server for build

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 // This is not needed in production
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   next();
