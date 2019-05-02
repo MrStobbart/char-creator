35c115429f694fe735c5e831c29e40565e87c5ca
Marek Meyer <marek.meyer@deutschebahn.com>
2019-05-02 10:18:01 +0200

    no bugfix Configured eslint and prettiert

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,33 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['react-app'],
+  extends: [
+    'react-app',
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // enables eslint-config-prettier and eslint-plugin-prettier
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
   rules: {
-    indent: ['warn', 2],
-    'max-len': ['warn', { code: 120 }],
     'no-use-before-define': ['off', { functions: false, classes: false, variables: false }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      {
+        allowTypedFunctionExpressions: true,
+        allowExpressions: true,
+      },
+    ],
   },
 };
